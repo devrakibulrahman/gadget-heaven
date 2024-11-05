a537f9e2f6567716b80f20565fa09922ae13872f
devrakibulrahman <devrakibulrahman@gmail.com>
2024-11-05 22:01:04 +0600

    home heading text add with responsive and modify navbar fixed some issues done on 11/5/2024 10:00 pm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,19 @@
 const Home = () => {
     return (
         <div className="relative w-full min-h-[100px] px-[15px]">
-            
+            <div className="w-full min-h-[100px] border-l border-r border-b border-[#e0e0e0] rounded-bl-2xl rounded-br-2xl pb-[6px] pl-[6px] pr-[6px]">
+                <div className="w-full min-h-[200px] bg-[#9538E2] rounded-bl-2xl rounded-br-2xl">
+                    <div className="container mx-auto px-4">
+                        <div className="w-full">
+                            <div className="w-full flex items-center justify-center">
+                                <div className="w-full text-center max-w-[1120px]">
+                                    <h1 className="font-sora text-[28px] leading-[41px] font-bold text-white md:text-[35px] md:leading-[54px] lg:text-[45px] lg:leading-[64px] xl:text-[56px] xl:leading-[72px]">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
