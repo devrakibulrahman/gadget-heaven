b2d7e56d9b41c7d050bcc194c381e69455d5d12c
devrakibulrahman <devrakibulrahman@gmail.com>
2024-11-05 14:20:35 +0600

    create a footer components add import in root.jsx file done on 11/5/2024 02:20 pm

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="w-full min-h-[500px] bg-white">
+            
+        </div>
+    );
+};
+
+export default Footer;
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,13 @@
-import Navbar from '../components/Navbar.jsx'
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
 
 const Root = () => {
     return (
         <div className="w-full min-h-screen bg-[#F7F7F7]">
             <Navbar></Navbar>
             <Outlet />
+            <Footer></Footer>
         </div>
     );
 };
