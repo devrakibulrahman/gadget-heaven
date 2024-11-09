100509b29d3d16c245538162fe42ffac4ec214b1
devrakibulrahman <devrakibulrahman@gmail.com>
2024-11-09 06:17:48 +0600

    added heading text in page heading components done on 11/9/2024 at 06:17

--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -1,8 +1,13 @@
 
 const PageHeading = () => {
     return (
-        <div>
-            
+        <div className={`w-full`}>
+            <div className={`w-full flex flex-col items-center justify-center pt-8`}>
+                <h1 className={`font-sora text-2xl leading-7 font-bold text-[#FFFFFF] md:text-[28px] md:leading-[34px] lg:text-[32px] lg:leading-10`}>Product Details</h1>
+                <div className={`w-full max-w-[795px] text-center mt-4`}>
+                    <p className={`font-sora text-sm leading-[26px] font-light text-[#FFFFFF] md:text-base`}>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import ProductDetailsCard from '../components/ProductDetailsCard';
+import PageHeading from '../components/PageHeading';
 
 const ProductDetails = () => {
 
@@ -8,12 +9,7 @@ const ProductDetails = () => {
                 <div className={`relative w-full min-h-[465px] bg-[#9538E2]`}>
                     <div className={`container mx-auto px-4`}>
                         <div className={`w-full relative`}>
-                            <div className={`w-full flex flex-col items-center justify-center pt-8`}>
-                                <h1 className={`font-sora text-2xl leading-7 font-bold text-[#FFFFFF] md:text-[28px] md:leading-[34px] lg:text-[32px] lg:leading-10`}>Product Details</h1>
-                                <div className={`w-full max-w-[795px] text-center mt-4`}>
-                                    <p className={`font-sora text-sm leading-[26px] font-light text-[#FFFFFF] md:text-base`}>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
-                                </div>
-                            </div>
+                            <PageHeading></PageHeading>
                             <div className={`w-full absolute top-[120%] left-0 lg:top-[130%]`}>
                                 <ProductDetailsCard></ProductDetailsCard>
                             </div>
