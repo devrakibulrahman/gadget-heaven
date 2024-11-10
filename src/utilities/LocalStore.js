f3036821d750f2373732cdb91e0b81cd51827f9d
devrakibulrahman <devrakibulrahman@gmail.com>
2024-11-10 13:58:58 +0600

    create local store js file for data store in browser local store done on 11/10/2024 at 01:58 pm

---
+++ src/utilities/LocalStore.js
@@ -0,0 +1,24 @@
+// data get from LocalStore ------------------------->
+const getDataFromCartToLocalStore = () => {
+    const getCartData = localStorage.getItem('cart');
+    if(getCartData) {
+        const cartData = JSON.parse(getCartData);
+        return cartData;
+    };
+    return [];
+};
+
+// data set To LocalStore --------------------------->
+const setDataToCartLocalStore = (cart) => {
+    const setCartData = JSON.stringify(cart);
+    localStorage.setItem("cart", setCartData);
+};
+
+// add data to LocalStore --------------------------->
+const addDataToLocalStore = (productId) => {
+    const dataFromLocalStore = getDataFromCartToLocalStore();
+    dataFromLocalStore.push(productId);
+    setDataToCartLocalStore(dataFromLocalStore);
+};
+
+export { getDataFromCartToLocalStore, setDataToCartLocalStore, addDataToLocalStore };
